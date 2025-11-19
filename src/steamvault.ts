51b157f3c27b343bec3a923b2125d9736f44b985
Moritz-Grimm <molumo@icloud.com>
2025-11-19 22:56:31 +0100

    add dotenvx import in steamvault.ts

--- src/steamvault.ts
+++ src/steamvault.ts
@@ -1,5 +1,6 @@
 import { loadConfigs } from "src/config-service";
 import { printMainMenu } from "./ui/menu-renderer";
+import "@dotenvx/dotenvx/config";
 
 await loadConfigs();
 await printMainMenu();
